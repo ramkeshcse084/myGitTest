4ec19113538a9650bfdd78a34b22b280c1d2a492
Ramkesh Singh <ramkeshcse084@gmail.com>
2018-07-23 22:04:33 +0530

    This is my ads

---
+++ app/src/main/java/com/nestedmango/mygit/Ads.java
@@ -0,0 +1,11 @@
+package com.nestedmango.mygit;
+
+/**
+ * Created by Ramkesh Singh on 23-07-2018.
+ */
+
+public class Ads {
+    public String Messa(){
+        return "Hii Nested Mango";// feature...ads..ui element...working with team
+    }
+}
